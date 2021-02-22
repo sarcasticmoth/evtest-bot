7991009a41f5b72b30abcff8c99a55d2ff2ad5e4
Vanessa Ulloa <veedotbebop@gmail.com>
2021-02-22 12:15:59 -0800

    add additional fields

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ import discord
 from discord.ext import commands
 
 token = os.getenv('DISCORD_BOT_TOKEN')
+client_user = os.getenv('CLIENT_USER')
+client_id = os.getenv('CLIENT_ID')
 bot = commands.Bot(command_prefix='$')
 
-# COMMANDS
+
+@bot.event
+async def on_ready():
+    print("----------------------")
+    print("Logged In As")
+    print("Username: {0}".format(client_user))
+    print("----------------------")
+
 
 @bot.command(name='test', help='this is a test command')
 async def test(ctx, arg):
     await ctx.send(arg)
+
 
 @bot.command(name='pikachu', help='this is a surprise pikachu response, custom for users')
 async def pikachu(ctx, *arg):
@@ -22,6 +32,7 @@ async def pikachu(ctx, *arg):
     else:
         await ctx.send('The family has not authorized you to use this command.')
 
+
 @bot.command(name='pikachuadmin')
 async def pikachu_admin(ctx, *arg):
     if str(ctx.author) == 'ctrl-vee#8271':
@@ -29,5 +40,6 @@ async def pikachu_admin(ctx, *arg):
         await ctx.send(file=discord.File('images/shocked_pikachu.png'))
     else:
         await ctx.send('only Vee can use this command.')
+
 
 bot.run(token)
